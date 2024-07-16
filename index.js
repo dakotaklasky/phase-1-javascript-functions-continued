34fc35713d8b5565cb34c86c7682821700d5bcb7
Dakota <dakota.klasky@gmail.com>
2024-07-15 20:59:31 -0400

    create functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(x = "roller-skate"){
+    return `This Saturday, I want to ${x}!`
+}
+
+//point of a function expression pointer??
+const mondayWork = function(x = 'go to the office'){
+    return `This Monday, I will ${x}.`
+}
+
+function wrapAdjective(y = "*"){
+    return function inner(x = "special"){
+        return `You are ${y}${x}${y}!`
+    }
+
+}
